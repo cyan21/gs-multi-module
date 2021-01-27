7e91277e4d1be0f7fcc88f656a5c845a9cd2d7e9
cyan21 <yann.chaysinh@gmail.com>
2021-01-27 10:08:27 +0100

    updated app version

--- complete/CI/bump_version.sh
+++ complete/CI/bump_version.sh
@@ -1,13 +1,12 @@
 #/!bin/bash
 
-cur_vers=1.0.0
-new_vers=1.1.0
-new_msg="I am watching a cooking show on TV"
+cur_vers=1.1.0
+new_vers=1.3.0
+new_msg="it's a raining day ..."
 
 # '' is for mac OS ...
 sed -i '' "s/<version>${cur_vers}<\/version>/<version>${new_vers}<\/version>/" "../application/pom.xml"
 sed -i '' "s/<version>${cur_vers}<\/version>/<version>${new_vers}<\/version>/" "../pom.xml"
 sed -i '' "s/<version>${cur_vers}<\/version>/<version>${new_vers}<\/version>/" "../library/pom.xml"
-sed -i '' "s/myDemoVersion: ${cur_vers}/myDemoVersion: ${new_vers}/" "jfrog/pipelines.steps.mvn.yaml"
 
 echo "service.message=$new_msg" > ../application/src/main/resources/application.properties
